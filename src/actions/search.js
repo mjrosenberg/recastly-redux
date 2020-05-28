136529e44cc42829da7a4e71e68cff880b18c533
lmodin <lisamodin@live.com>
2020-05-28 16:51:54 -0700

    Updating with first two action tests passing. Syntax issue is blocking search you tube test from completing.

--- src/actions/search.js
+++ src/actions/search.js
@@ -5,8 +5,20 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
 var handleVideoSearch = (q) => {
- 
   //TODO:  Write an asynchronous action to handle a video search!
+    //should call the search function, to call to youtube based on the search entry field
+  //call searchYouTube, pass in: api key as key and value in search field as query
+
+  return ((dispatch) => {
+    searchYouTube({YOUTUBE_API_KEY, q}, (videos)=>{
+      //call dispatch on each change needed
+      // dispatch(changeVideo(videos[0]))
+      // dispatch(changeVideoList(videos))
+      return null;
+    });
+    //return null;
+  })
+
 };
 
 export default handleVideoSearch;

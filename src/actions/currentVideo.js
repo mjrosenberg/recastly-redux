2fa87a395f0b12419fd5ed33b3f8721c0bca22a0
lmodin <lisamodin@live.com>
2020-05-28 12:45:54 -0700

    First tests passing for currentVideo and VideoList by returning the type of change and passing through the parameter of video or videos

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,5 +1,13 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
+
+  //when a title is clicked, currentVideo state should change
+  //when a search is implemented, currentVideo state should change to 1st video in list
+
+  //define the type as 'CHANGE_VIDEO;
+  type: 'CHANGE_VIDEO',
+  //define a video key as the video passed in
+  video
 });
 
 export default changeVideo;
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -1,5 +1,13 @@
 var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
+
+  //when a search is implemented, videoList should create a list element for each video in the array
+
+  //define a CHANGE_VIDEO_LIST value
+  type: 'CHANGE_VIDEO_LIST',
+  //pass in videos as defined in argument
+  videos
+
 });
 
 export default changeVideoList;
